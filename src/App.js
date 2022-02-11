bbaf49b637472a4e24c203f85bef59a2140d8dde
AX Concepts <93191217+alexyhtsoi@users.noreply.github.com>
2022-02-11 16:12:54 +0800

    ax-concept ver1.0
    Ifrst upload to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>GIT Commands</h1>
+      <p>git init, git status, git commit -m "description of the commit", git log, git commit --amend -m "update", git reflog</p>
+    </>
   );
 }
 
